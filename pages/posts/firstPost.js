12358124dd152414e13aa973546044991b53f063
yuto <h.yyt9n@gmail.com>
2022-09-22 15:20:32 +0900

    headコンポーネント実装

--- pages/posts/firstPost.js
+++ pages/posts/firstPost.js
@@ -1,8 +1,13 @@
+import Head from "next/head";
 import Link from "next/link";
+
 
 export default function FirstPost() {
     return (
         <div>
+            <Head>
+                <title>Post #1</title>
+            </Head>
             <h1>最初の投稿</h1>
             <Link href="/">ホームへ戻る</Link>
         </div>
