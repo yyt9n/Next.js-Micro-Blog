5ad06171cdf3a13137bdbb0152d5bf8bf65908fa
yuto <h.yyt9n@gmail.com>
2022-09-22 15:15:57 +0900

    Linkコンポーネントでのページ遷移実装

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+import Link from 'next/link'
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -13,7 +15,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <Link href="/posts/firstPost">Next.js!</Link>
         </h1>
 
         <p className={styles.description}>
