b0f7cdde045d07c8c571440e52d8839d81468525
yuto <h.yyt9n@gmail.com>
2022-09-22 16:48:02 +0900

    css反映

--- components/Layout.js
+++ components/Layout.js
@@ -1,19 +1,21 @@
 import Head from "next/head";
 import Image from "next/image";
+import styles from './layout.module.css';
+import utilStyles from '../styles/utils.module.css';
 
-const name = "yyt9n"
+const name = "@ yyt9n"
 
 export const siteTitle = "Next.js blog"
 
 function Layout({ children }) {
     return (
-        <div>
+        <div className={styles.container}>
             <Head>
                 <link rel="icon" href="/images/profile.png" />
             </Head>
-            <header>
-                <Image src="/images/profile.png" alt="Profile Logo" width={72} height={72} />
-                <h1>{name}</h1>
+            <header className={styles.header}>
+                <Image src="/images/profile.png" alt="Profile Logo" width={72} height={72} className={utilStyles.borderCircle} />
+                <h1 className={utilStyles.heading2Xl}>{name}</h1>
                 <main>{children}</main>
             </header>
         </div>
